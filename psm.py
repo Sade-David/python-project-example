aa72ee58165b243436d6d967ef916bfeb57eaca0
Sade-David <91186491+Sade-David@users.noreply.github.com>
2026-01-30 10:23:39 +0200

    Delete psm.py

--- psm.py
+++
@@ -1,6 +0,0 @@
-class PsmMod:
-
-    def __init__(self, initial_state: str = "PSM"):
-        # Member to track the device's status
-        self.current_state = initial_state
-        self.serial_interface = None
